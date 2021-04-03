0d41f745505c3677952504ad2da907d49cdf3da0
Chawin Aiemvaravutigul <nick41746@hotmail.com>
2021-04-03 12:55:13 +0700

    fix

--- git_check_branch.bat
+++ git_check_branch.bat
@@ -4,7 +4,7 @@ git rev-parse --abbrev-ref HEAD 1> %HOMEDRIVE%\%HOMEPATH%\Scripts\tmp 2> nul
 set /p GITBRANCH=<%HOMEDRIVE%\%HOMEPATH%\Scripts\tmp
 
 set GITUNTRACK=
-git status -s 1> %HOMEDRIVE%\%HOMEPATH%\Scripts\tmp 2> nul
+git status -s 2> nul | head -n 1 1> %HOMEDRIVE%\%HOMEPATH%\Scripts\tmp
 set /p GITUNTRACK=<%HOMEDRIVE%\%HOMEPATH%\Scripts\tmp
 
 if not "%GITUNTRACK%" == "" (
@@ -12,7 +12,7 @@ if not "%GITUNTRACK%" == "" (
 )
 
 if "%GITBRANCH%" == "" (
-    prompt $E[0;1;40m[ $E[1;36;40m%username% $E[0;1;40m:$s$E[1;40;32m$t$h$h$h $E[0;1;40m] $E[31m~ $E[35;40m$P$_$E[0;1;40m$G$s
+    @SET "prompt=%CONDA_PROMPT_MODIFIER%$E[0;1;40m[ $E[1;36;40m%username% $E[0;1;40m:$s$E[1;40;32m$t$h$h$h $E[0;1;40m] $E[31m~ $E[35;40m$P$_$E[0;1;40m$G$s"
 ) else (
-    prompt $E[0;1;40m[ $E[1;36;40m%username% $E[0;1;40m:$s$E[1;40;32m$t$h$h$h $E[0;1;40m] $E[31m~ $E[35;40m$P$_$E[0;1;40m$C$E[1;33;40m%GITBRANCH%$E[0;1;40m$F$s$G$s
+    @SET "prompt=%CONDA_PROMPT_MODIFIER%$E[0;1;40m[ $E[1;36;40m%username% $E[0;1;40m:$s$E[1;40;32m$t$h$h$h $E[0;1;40m] $E[31m~ $E[35;40m$P$_$E[0;1;40m$C$E[1;33;40m%GITBRANCH%$E[0;1;40m$F$s$G$s"
 )
