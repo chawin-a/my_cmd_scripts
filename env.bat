f8efdc7d9492151db09fe12b52a104fb8668ac92
Chawin Aiemvaravutigul <nick41746@hotmail.com>
2021-04-03 20:56:10 +0700

    fix

--- env.bat
+++ env.bat
@@ -9,6 +9,7 @@ DOSKEY git=%HOMEDRIVE%\%HOMEPATH%\Scripts\git_command.bat $*
 DOSKEY cd=%HOMEDRIVE%\%HOMEPATH%\Scripts\change_dir $*
 DOSKEY cpp_run=%HOMEDRIVE%\%HOMEPATH%\Scripts\cpp_run.bat $1 $2
 DOSKEY cpp_run_d=%HOMEDRIVE%\%HOMEPATH%\Scripts\cpp_run_d.bat $1 $2
+DOSKEY stress=%HOMEDRIVE%\%HOMEPATH%\Scripts\stress_test_script.cmd $1 $2 $3
 DOSKEY exit=exit 0
 DOSKEY clear=cls
 
